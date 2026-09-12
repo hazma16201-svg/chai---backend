3d130ac3304f27a82e71ce69c854b1c4fb2b3f1d
Ameer Hamza <hazma16201@gmail.com>
2026-09-12 16:40:26 +0500

    Initial commit - Production Grade Backend Setup

---
+++ public/temp/src/middlewares/multer.middleware.js
@@ -0,0 +1,32 @@
+import multer from "multer";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+
+  export const verifyJWT = asyncHandler(async(req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+      
+        console.log(token);
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+  
+export const upload = multer({ 
+    storage, 
+})
